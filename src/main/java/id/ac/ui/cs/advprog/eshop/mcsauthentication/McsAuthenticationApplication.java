77d3362a2a00b46b200b6eef35525f06c10380ca
Anisa Maharani <anismyes@gmail.com>
2024-04-14 23:55:44 +0800

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsauthentication/McsAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcsauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/mcsauthentication/McsAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcsauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
