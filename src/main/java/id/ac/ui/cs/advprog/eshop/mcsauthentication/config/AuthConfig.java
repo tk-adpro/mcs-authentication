4d72b57c1df647bff5b201f8e2dbbbedc0f1d6b9
Anisa Maharani <anismyes@gmail.com>
2024-04-16 22:37:56 +0800

    [GREEN] Create auth config and UserServiceImpl implementation

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsauthentication/config/AuthConfig.java
@@ -0,0 +1,54 @@
+package id.ac.ui.cs.advprog.eshop.mcsauthentication.config;
+
+import id.ac.ui.cs.advprog.eshop.mcsauthentication.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class AuthConfig {
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsauthentication/service/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package id.ac.ui.cs.advprog.eshop.mcsauthentication.service;
+
+import id.ac.ui.cs.advprog.eshop.mcsauthentication.dto.request.SignupRequest;
+import id.ac.ui.cs.advprog.eshop.mcsauthentication.model.Role;
+import id.ac.ui.cs.advprog.eshop.mcsauthentication.model.User;
+import id.ac.ui.cs.advprog.eshop.mcsauthentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User createRequestedUserWithRoles(SignupRequest signupRequest, Set<Role> roles) {
+        User user = new User(
+                signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                encoder.encode(signupRequest.getPassword()),
+                signupRequest.getFirstName(),
+                signupRequest.getLastName(),
+                signupRequest.getDob());
+
+        user.setRoles(roles);
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    }
+}
